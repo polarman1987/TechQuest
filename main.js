bbfc1c27a0359c0273880254818d6e5a4197c86e
Andrew Nicolaou <andrewcnicolaou@gmail.com>
2017-11-13 16:53:15 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var questions = [
+    [ "When was the first generation iPhone released?", "2007", "2006", "2005", "A" ],
+	[ "The technologically advanced humanoid robot ASIMO is made by which car company?", "Toyota", "Honda", "Nissan", "B" ],
+	[ "Solar power generates electricity from what source?", "The Sun", "Wind", "Waves", "A" ],
+	[ "Firefox, Opera, Chrome, Safari and Explorer are types of what?", "Operating Systems", "Search Engines", "Word Processors", "B" ],
+	[ "In terms of computing, what does ROM stand for?", "Random Operating Memory", "Read Only Memory", "Random Only Memory", "B" ]
+];
+function _(x){
+	return document.getElementById(x);
+}
+function renderQuestion(){
+	test = _("test");
+	if(pos >= questions.length){
+		test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct!</h2>";
+		_("test_status").innerHTML = "Test Completed.";
+		pos = 0;
+		correct = 0;
+		return false;
+	}
+	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+	question = questions[pos][0];
+	chA = questions[pos][1];
+	chB = questions[pos][2];
+	chC = questions[pos][3];
+	test.innerHTML = "<h3>"+question+"</h3>";
+	test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br/>";
+	test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br/>";
+	test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br/><br/>";
+	test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+	choices = document.getElementsByName("choices");
+	for(var i=0; i<choices.length; i++){
+		if(choices[i].checked){
+			choice = choices[i].value;
+		}
+	}
+	if(choice == questions[pos][4]){
+		correct++;
+	}
+	pos++;
+	renderQuestion();
+}
+window.addEventListener("load", renderQuestion, false);
